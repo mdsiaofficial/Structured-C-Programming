038090dcff23c94f4ab516191478af056b0ca946
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-13 23:23:34 +0600

    x

--- A_Taking_Input/9.c
+++ A_Taking_Input/9.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
 
+    int x,y;
+    int p;
+    printf("Enter the values: ");
+
+    scanf("%d %d", &x, &y);
+	
+	p=pow(x,y);
+
+    printf("%d\n", p);
+    
     return 0;
 }
