681ef373bfd660f2bcd08ab46a730f6f243577ab
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-13 22:34:01 +0600

    vv

--- A_Taking_Input/2.cpp
+++ A_Taking_Input/2.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main()
 {
-
-    
+    float len, wi, peri, area;
+    cin >> len >> wi;
+    peri = (len + wi) * 2;
+    area = len * wi;
+    cout << "Perimeter: " << peri << endl<<"Area: " << area << endl;
     return 0;
 }
