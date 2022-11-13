579cc2d282e249a9aa1ed1adb30cf18d4c12e9d6
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-13 22:40:56 +0600

    v

---
+++ A_Taking_Input/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define pi 3.14159
+int main()
+{
+    float r;
+    cin >> r;
+    float d, c, a;
+    d =
+    c = 
+    a = pi * r * r;
+
+    return 0;
+}
