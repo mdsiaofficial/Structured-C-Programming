bbbd9cb6af79fe0306c2a227354abd8df847412d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-14 23:43:44 +0600

    vvvv

---
+++ A_Taking_Input/9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+int main(){
+
+    int x,y;
+    int p;
+    
+    cout << "Enter the values: " << endl;
+    
+    cout << x >> y;
+    p = pow(x, y);
+
+    
+    cout << p << endl;
+    return 0;
+}
