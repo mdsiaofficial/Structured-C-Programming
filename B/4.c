edfe21ff1bcd6548f68e70b341fbff7f3a696368
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-14 23:39:18 +0600

    dddd

--- B/4.c
+++ B/4.c
@@ -4,6 +4,7 @@ int main(){
 
     //int x = 5;
     char x = 'g';
+
     /*
     if((x>='a' && x<='z') || (x>='A' && x<='Z')){
         printf("This is an alphabet.");
@@ -11,7 +12,8 @@ int main(){
         printf("This is not an alphabet.");
     }
     */
-    
+
+    // ternary conditional
     ((x>='a' && x<='z') || (x>='A' && x<='Z')) ? printf("This is an alphabet.") : printf("This is not an alphabet.");
     
     
