24155cbc8d3a684163e68c8f0ec5d117024dc8dd
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-19 00:59:36 +0600

    ff

---
+++ C/2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    if(n%5==0 && n%11==0){
+        printf("Divible by 5 and 11\n");
+    }else{
+        printf("Not Divisible by 5 and 11\n");
+    }
+    return 0;
+}
