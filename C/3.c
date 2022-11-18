0846c8ec3d32f530cae8fd28eb98256b427c2de3
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-19 01:06:38 +0600

    xx

--- C/3.c
+++ C/3.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-
+    
+    int a[8] = {500, 100, 50, 20, 10, 5, 2, 1};
+    int Amount, i, temp;
+    
+    printf("Enter the Amount of Money  =  ");
+    scanf("%d", &Amount);
+    
+    temp = Amount;
+    
+    for(i = 0; i < 8; i++)
+    {
+        printf("\n %d Notes is = %d", a[i], temp / a[i]);
+        temp = temp % a[i];
+    }
     return 0;
 }
