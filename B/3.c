fe033c03038e9525bfe4bd7cdcd80d64b3571157
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-19 00:54:14 +0600

    ddd

--- B/3.c
+++ B/3.c
@@ -4,7 +4,7 @@ int main(){
     int year;
     scanf("%d", &year);
 
-    (year % 400 == 0 || (year % 4 == 0 && year%100 !=0)) ? printf("%d Leap Year", year) : printf("%d Not Leap Year", year);
-    
+    (year % 400 == 0 || (year % 4 == 0 && year%100 !=0)) ? printf("%d Leap Year\n", year) : printf("%d Not Leap Year\n", year);
+
     return 0;
 }
---
+++ C/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    if(n==0){
+        printf("Zero\n");
+    }else if(n>0){
+        printf("Positive\n");
+    }else{
+        printf("Negative\n");
+    }
+    return 0;
+}
