aee1e4e001de5ec49c01b07116b831d8f995b123
Md Shoriful Islam <mdsiaofficial@gmail.com>
2022-11-20 23:33:54 +0600

    ii

---
+++ C/5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    char c;
+    int lowercase_vowel, uppercase_vowel;
+    printf("Enter an alphabet: ");
+    scanf("%c", &c);
+
+    
+    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    // evaluates to 1 (true) if c is a vowel
+    if (lowercase_vowel || uppercase_vowel)
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+    return 0;
+}
