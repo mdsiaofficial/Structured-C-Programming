7730fde1041f105ac7ad909d93fac8ea6ae0ac45
Md Shoriful Islam <mdsiaofficial@gmail.com>
2022-11-20 22:08:07 +0600

    ddd

--- C/3.c
+++ C/3.c
@@ -2,7 +2,17 @@
 #include <math.h>
 int main(){
     
-    int a[8] = {500, 100, 50, 20, 10, 5, 2, 1};
+    //int a[8] = {500, 100, 50, 20, 10, 5, 2, 1};
+    
+    int tk500 = 500;
+    int tk100 = 100;
+    int tk50 = 50;
+    int tk20 = 20;
+    int tk10 = 10;
+    int tk5 = 5;
+    int tk2 = 2;
+    int tk1 = 1;
+
     int Amount, i, temp;
     
     printf("Enter the Amount of Money  =  ");
