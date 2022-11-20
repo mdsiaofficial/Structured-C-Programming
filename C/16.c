d8b38c0637acf9c6aff82488866e214f39343abe
Md Shoriful Islam <mdsiaofficial@gmail.com>
2022-11-20 23:39:22 +0600

    ddd

---
+++ C/16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    float a;
+    scanf("%f", &a);
+
+    if(a>=10000 && a<=20000){
+        a=a+(a*0.2)+(a*0.8);
+    }else if(a>=20000 && a<=30000){
+        a=a+(a*0.25)+(a*0.9);
+    }else if(a>=30000){
+        a=a+(a*0.3)+(a*0.95);
+    }
+
+    printf("%.2f", a);
+    return 0;
+}
