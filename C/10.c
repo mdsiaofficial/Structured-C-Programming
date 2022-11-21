4888c748ace1ad525cc9cd20ad960b6b09faa5cc
Md Shoriful Islam <mdsiaofficial@gmail.com>
2022-11-21 07:16:21 +0600

    Create 10.c

---
+++ C/10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+  
+int main()
+{
+    int angle1, angle2, angle3, Sum;
+  
+    printf("\n Please Enter Three Angles of a Triangle : ");
+    scanf("%d%d%d", &angle1, &angle2, &angle3);
+     
+    Sum = angle1 + angle2 + angle3;
+     
+    if(Sum == 180)
+    {
+        printf("\n This is a Valid Triangle");
+    }
+    else
+    {
+        printf("\n This is an Invalid Triangle");
+    }  
+    return 0;
+ }
