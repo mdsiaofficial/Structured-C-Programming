6e91d5407cbaf75052f11c4241dd08d333843144
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-23 17:36:32 +0600

    cc

--- D/3.c
+++ D/3.c
@@ -2,9 +2,36 @@
 
 int main()
 {
-    int week;
-    printf("Enter week number(1-7): ");
-    scanf("%d", &week);
-    
+    int month;
+    printf("Enter month number(1-12): ");
+    scanf("%d", &month);
+
+    switch(month)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12: 
+            printf("31 days");
+            break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11: 
+            printf("30 days");
+            break;
+
+        case 2: 
+            printf("28/29 days");
+            break;
+
+        default: 
+            printf("Invalid input! Please enter month number between 1-12");
+    }
+
     return 0;
 }
