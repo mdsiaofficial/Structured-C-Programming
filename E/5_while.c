7f5545282bb9141bb0074d68010aa72249121cab
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-25 23:52:45 +0600

    xx

---
+++ E/5_while.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+  int i, Number, count, Minimum, Maximum; 
+  printf("\n Please Enter the Minimum & Maximum Values\n");
+  scanf("%d %d", &Minimum, &Maximum);
+  Number = Minimum; 
+  printf("Prime Numbers Between %d and %d are:\n", Minimum, Maximum);    
+  while(Number <= Maximum)
+  {
+    count = 0;
+    i = 2;
+    while(i <= Number/2)
+    {
+      if(Number%i == 0)
+      {
+        count++;
+	break;
+      }
+      i++;	
+    }	
+    if(count == 0 && Number != 1 )
+    {
+       printf(" %d ", Number);
+    }
+    Number++; 
+  }
+  return 0;
+}
