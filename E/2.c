0fb19d5964a4171313275e5dc4bc1c92c6773ab5
mdsiaofficial <mdsiaofficial@gmail.com>
2022-11-25 23:41:18 +0600

    dd

--- E/2.c
+++ E/2.c
@@ -2,6 +2,24 @@
 
 int main()
 {
+    //char ch = 'a';
+    char ch;
+    scanf("%c",&ch);
 
+    printf("Alphabets from a - z are: \n");
+    if(ch>='a' && ch<='z'){
+        while(ch<='z')
+        {
+            printf("%c\n", ch);
+            ch++;
+        }
+    }else{
+        while(ch<='Z')
+        {
+            printf("%c\n", ch);
+            ch++;
+        }
+    }
+    
     return 0;
 }
