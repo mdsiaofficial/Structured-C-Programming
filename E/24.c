525163ca6494ab5cd6b283b6b68ec9aa79020095
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-01 22:45:37 +0600

    dd

---
+++ E/24.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+
+    long long number;
+    scanf("%lld", &number);
+
+    long long n[10];
+    for(int i = 0; i<10; i++){
+        n[i]=0;
+    }
+
+    //c=2
+
+    //int a[10]={0,1,2,3,2,1,0,0,0,0}
+
+
+
+    int last;
+    while (number!=0)
+    {
+
+        last=number%10;
+        n[last]++;
+        number=number/10;
+        
+    }
+    
+    for(int i=0; i<10; i++){
+        printf("Frequency of %d = %d\n", i, n[i]);
+
+    }
+    return 0;
+}
---
+++ E/27.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+long long factorial(int n){
+    long long fact=1;
+
+    for(int i=n; i>=1; i--){
+        fact=fact*i;
+    }
+
+    return fact;
+}
+
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    double sum=0.0;
+
+    
+    for(int i=1; i<=n; i++){
+        sum = sum + ((double)i/(double)factorial(i));
+    }
+
+
+    printf("Sum of the series: %lf\n", sum);
+    
+    return 0;
+}
