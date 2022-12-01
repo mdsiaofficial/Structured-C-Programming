2f0665d33368b6b6e99e3eb57914b7123204c3a9
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-01 21:59:03 +0600

    xxx

---
+++ E/16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+
+    long long number;
+    scanf("%lld", &number);
+
+    long long n[10];
+    for(int i = 0; i<10; i++){
+        n[i]=0;
+    }
+
+    int last;
+    while (number!=0)
+    {
+
+        last=number%10;
+        n[last]++;
+        number=number/10;
+        
+    }
+    
+    for(int i=0; i<10; i++){
+        printf("Frequency of %d = %d\n", i, n[i]);
+
+    }
+    return 0;
+}
