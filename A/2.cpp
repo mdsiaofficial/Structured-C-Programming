8f78160287d8c085a6d14a41358207b6250252e3
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-02 23:37:40 +0600

    ii

--- A/2.cpp
+++ A/2.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main()
 {
     float len, wi, peri, area;
+    
     cin >> len >> wi;
     peri = (len + wi) * 2;
     area = len * wi;
     cout << "Perimeter: " << peri << endl<<"Area: " << area << endl;
-    //
+    
     
     return 0;
 }
