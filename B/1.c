7fba2cba5e8d002748fbcd7b9a2a4b63d57806f7
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-03 22:34:19 +0600

    cccc

--- B/1.c
+++ B/1.c
@@ -9,37 +9,25 @@ int main()
     scanf("%d%d%d", &num1, &num2, &num3);
 
 
-    if((num1 > num2) && (num1 > num3))
-    {
+    if(num1 > num2 && num1 > num3){
         /* If num1 is greater than both */
         max = num1;
-    }
-    else if((num2 > num1) && (num2 > num3))
-    {
+    }else if(num2 > num1 && num2 > num3){
         /* If num2 is greater than both */
         max = num2;
-    }
-    else if((num3 > num1) && (num3 > num2))
-    {
+    }else if(num3 > num1 && num3 > num2){
         /* If num3 is greater than both */
         max = num3;
     }
 
-    if((num1 < num2) && (num1 < num3))
-    {
-
+    if(num1 < num2 && num1 < num3){
         min = num1;
-    }
-    else if((num2 < num1) && (num2 < num3))
-    {
-
+    }else if(num2 < num1 && num2 < num3){
         min = num2;
-    }
-    else if((num3 < num1) && (num3 < num2))
-    {
-
+    }else if(num3 < num1 && num3 < num2){
         min = num3;
     }
+    
     /* Print maximum number */
     printf("Maximum among all three numbers = %d\n", max);
     printf("Minimum among all three numbers = %d\n", min);
--- C/8.c
+++ C/8.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
- 
+#include <string.h>
 int main() 
 {
  
-    char *(a[7])={"monday","tuesday","wednesday","thursday","friday","saturday","sunday"},m; 
+    //char *(a[7])={"monday","tuesday","wednesday","thursday","friday","saturday","sunday"},m; 
+    string a[7]={"saturday","sunday","monday","tuesday","wednesday","thursday","friday"}; 
     printf("Enter the week day:");
- 
-    scanf("%d",&m);
+    int x;
+    scanf("%d",&x);
+/*  scanf("%d",&m);
     if(m>7 || m<1){
     	printf("invalid input");
     }else}
         printf("%s",a[m-1]);
     }
+ */
+    if(x>7 || x<1){
+    	printf("invalid input");
+    }else}
+        printf("%s", a[x-1]);
+    }
+
    return 0;
 }
