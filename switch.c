ee44ca6a4e2929ddde19137952138a91de3ff48f
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-04 20:55:26 +0600

    cc

--- switch.c
+++ switch.c
@@ -1,44 +1,59 @@
 #include <stdio.h>
 int main(){
 
+    int a,b;
+    char x;
+    scanf("%d %c %d", &a,&x,&b);
+
+    switch (x)
+    {
+        case '+':
+            printf("Sum of %d and %d = %d",a,b,(a+b));
+            break;
+        case '-':
+            printf("Substraction of %d and %d = %d",a,b,(a-b));
+            break;
+        case '*':
+            printf("Product of %d and %d = %d",a,b,(a*b));
+            break;
+        case '/':
+            printf("Quotient of %d and %d = %d",a,b,(a/b));
+            break;
+        case '%':
+            printf("Modulas of %d and %d = %d",a,b,(a%b));
+            break;
+        default:
+            printf("Invalid Input");
+            break;
+    }
+
+
+
+
+/* 
     char ch;
     scanf("%c", &ch);
 
     switch (ch)
     {
-    case 'a':
-        printf("Vowel");
-        break;
-    case 'e':
-        printf("Vowel");
-        break;
-    case 'i':
-        printf("Vowel");
-        break;
-    case 'o':
-        printf("Vowel");
-        break;
-    case 'u':
-        printf("Vowel");
-        break;
-    case 'A':
-        printf("Vowel");
-        break;
-    case 'E':
-        printf("Vowel");
-        break;
-    case 'I':
-        printf("Vowel");
-        break;
-    case 'O':
-        printf("Vowel");
-        break;
-    case 'U':
-        printf("Vowel");
-        break;
-    
-    default:
-        printf("Consonant");
-        break;
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("Vowel");
+            break;
+        
+        default:
+            printf("Consonant");
+            break;
     }
+
+ */
+
 }
