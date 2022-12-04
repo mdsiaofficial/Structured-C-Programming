82758c1f1e403a9f66e06af2ee6f62ec5a8b7854
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-04 08:49:59 +0600

    xddd

--- B/2.c
+++ B/2.c
@@ -3,7 +3,7 @@
 int main()  {
 
     int x;
-    printf("Enter the number to find Even or Odd");
+    printf("Enter the number to find Even or Odd: ");
     scanf("%d", &x);
 
     (x % 2 == 0) ? printf("Even") : printf("Odd");
