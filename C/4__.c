c146a59dd34cded016e92ff1bf586b4a00df684d
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-13 15:02:05 +0600

    ujuj

--- C/4__.c
+++ C/4__.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    
+    char f;
+    scanf("%c", &f);
+
+    if((f>='a' && f<='z') || (f>='A' && f<='Z')){
+        printf("%c is an Alphabet", f);
+    }else{
+        printf("%c is not an Alphabet", f);
+    }
+
     return 0;
 }
-
