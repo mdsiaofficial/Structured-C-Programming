dbef570596c29edf2678e9ce7f0e76e825b9083c
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-13 16:10:21 +0600

    vvv

---
+++ C/5__.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+    char x;
+
+    scanf("%c", &x);
+
+    if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
+
+        printf("%c is a Vowel", x);
+        
+    }else if(x=='A' || x=='E' ||x=='I' || x=='O' || x=='U'){
+
+        printf("%c is a Vowel", x);
+
+    }else{
+
+        printf("%c is a Consonant", x);
+    }
+
+    return 0;
+}
+
