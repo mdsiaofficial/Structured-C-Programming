b1431883d2b86e3a20a080c641d0bb40982eed39
mdsiaofficial <mdsiaofficial@gmail.com>
2022-12-13 16:31:25 +0600

    ccc

---
+++ C/6__.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+    char x;
+    scanf("%c", &x);
+
+    if(x>='0' && x<='9'){
+
+        printf("%c is a Digit/Number", x);
+
+    }else if((x>='a' && x<='z') || (x>='A' && x<='Z')){
+
+        printf("%c is an Alphabet", x);
+    }else{
+
+        printf("%c is a Special Character", x);
+    }
+
+
+    return 0;
+}
