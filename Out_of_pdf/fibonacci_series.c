3fe8606fefaadf5e6767bfd201f685826cd09d15
mdsiaofficial <mdsiaofficial@gmail.com>
2023-01-14 00:27:52 +0600

    vv

--- Out_of_pdf/fibonacci_series.c
+++ Out_of_pdf/fibonacci_series.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 // The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    int x=0;
-    for(int i=1; i<n; i++){
-        printf("%d, ", x);
-        x=x+i;
-    }
-    return 0;
+#include <stdio.h>
+int main() {
+
+  int i, n;
+
+  // initialize first and second terms
+  int t1 = 0, t2 = 1;
+
+  // initialize the next term (3rd term)
+  int nextTerm = t1 + t2;
+
+  // get no. of terms from user
+  printf("Enter the number of terms: ");
+  scanf("%d", &n);
+
+  // print the first two terms t1 and t2
+  printf("Fibonacci Series: %d, %d, ", t1, t2);
+
+  // print 3rd to nth terms
+  for (i = 3; i <= n; ++i) {
+    printf("%d, ", nextTerm);
+    t1 = t2;
+    t2 = nextTerm;
+    nextTerm = t1 + t2;
+  }
+
+  return 0;
 }
