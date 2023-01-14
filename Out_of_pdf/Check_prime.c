dd0f3e972e0319fd75870594fa135f81c5414e60
Md Shoriful Islam <mdsiaofficial@gmail.com>
2023-01-14 07:52:58 +0600

    Create Check_prime.c

---
+++ Out_of_pdf/Check_prime.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    int num, j, c;
+ 
+    printf("Enter a number: \n");
+    scanf("%d", &num);
+ 
+    if (num <= 1)
+    {
+        printf("%d is not a prime number \n", num);
+        exit(1);
+    }
+    c = 0;
+ 
+    // To check prime number
+    for (j = 2; j <= num / 2; j++)
+    {
+        if ((num % j) == 0)
+        {
+            c = 1;
+            break;
+        }
+    }
+    if (c == 0)
+        printf("%d is a prime number \n", num);
+     else
+        printf("%d is not a prime number \n", num);
+
+    return 0;
+}
