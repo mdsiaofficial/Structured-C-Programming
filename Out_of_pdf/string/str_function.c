c41695c45377c7be6ad39d64ae1c90c2b1ec6b89
mdsiaofficial <mdsiaofficial@gmail.com>
2023-01-20 16:25:40 +0600

    d

--- Out_of_pdf/string/str_function.c
+++ Out_of_pdf/string/str_function.c
@@ -4,36 +4,36 @@ int main(){
 
     // 1. strlen
     char variable[] = {"I do programming"};
-    printf("%d\n\n", strlen(variable));
+    printf("%d\n", strlen(variable));
 
     // 2. strcpy
     char cpytext[100];
     char vaar[] = {"Programming is awesome"};
     strcpy(cpytext, vaar);
-    printf("%s\n\n", cpytext);
+    printf("%s\n", cpytext);
     
     // 3. strcat
     char daar[] = {"I am cse student, "};
     char taar[] = {"I do programming"};
     strcat(daar, taar);
-    printf("%s\n\n", daar);
+    printf("%s\n", daar);
     
     // 4. strcmp
     char bar[] = {"Bulbul"};
     char jar[] = {"Bulbol"};
-    printf("%d\n\n", strcmp(bar, jar));
+    printf("%d\n", strcmp(bar, jar));
     
     // 5. strrev
     char wee[] = {"Ashiq"}; // niraj
-    printf("%s\n\n", strrev(wee));
+    printf("%s\n", strrev(wee));
     
     // 6. strlwr
     char text[] = {"My Name Is Ashiq"};
-    printf("%s\n\n", strlwr(text));
+    printf("%s\n", strlwr(text));
     
     // 7. strupr
     char text2[] = {"My Name Is Ashiq"};
-    printf("%s\n\n", strupr(text2));
+    printf("%s\n", strupr(text2));
     
     return 0;
 }
