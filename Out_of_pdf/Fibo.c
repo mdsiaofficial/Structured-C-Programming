483bdd05666f250499b38976ea0cf31f5d57c564
mdsiaofficial <mdsiaofficial@gmail.com>
2023-01-22 10:25:57 +0600

    ddd

--- Out_of_pdf/Fibo.c
+++ Out_of_pdf/Fibo.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int fibbonacci(int n) {
-   if(n == 0){
-      return 0;
-   }else if(n == 1) {
-      return 1;
-   }else {
-      return (fibbonacci(n-1) + fibbonacci(n-2));
-   }
+    if(n == 0){
+        return 0;
+    }else if(n == 1) {
+        return 1;
+    }else {
+        return (fibbonacci(n-1) + fibbonacci(n-2));
+    }
 }
 
 int main() {
