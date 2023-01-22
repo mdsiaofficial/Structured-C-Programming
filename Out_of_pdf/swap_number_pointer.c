1983b45574d21a150b9cc71cf95f95bca01643db
mdsiaofficial <mdsiaofficial@gmail.com>
2023-01-22 10:22:31 +0600

    hhh

--- Out_of_pdf/swap_number_pointer.c
+++ Out_of_pdf/swap_number_pointer.c
@@ -2,7 +2,20 @@
 
 int main()
 {   
+    int a,b;
     
+    printf("Enter value of A: ");
+    scanf("%d", &a);
+    printf("Enter value of B: ");
+    scanf("%d", &b);
+    
+    int *p1=&a, *p2=&b;
+
+    *p1=*p1+*p2;
+    *p2=*p1-*p2;
+    *p1=*p1-*p2;
+    
+    printf("Now swap: A= %d and B= %d\n",*p1,*p2);
     
     return 0;
 }
