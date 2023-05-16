c7bf27db9390b4646c7d23d52a7aface5e04aba1
mdsiaofficial <mdsiaofficial@gmail.com>
2023-05-16 23:38:25 +0600

    aaaa

--- Untitled2.c
+++ Untitled2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 float NewArr(float narr[]){
-    for(int )
-    float avrg
+    float s=0;
+    for(int i=0; i<sizeof(narr); i++){
+        s=s+narr[i];
+    }
+    float x=sizeof(narr)-1;
+    float avrg=s/x;
     return avrg;
 }
 int main()
 {
-    float arr[7]={3.4,4.5,2.6,6.5,8.9,3.6,4.7}
-
-
+    float arr[7]={3.4,9.5,2.6,6.5,8.9,3.6,4.7};
+    printf("%.2f\n", NewArr(arr));
     return 0;
 }
